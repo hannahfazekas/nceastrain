6cf29dc4fdb22046beb89c44939d925cf1e26efb
Hannah Fazekas <hannah.fazekas@unh.edu>
2020-02-20 15:38:12 -0800

    writing functions

---
+++ my_function_pct.R
@@ -0,0 +1,15 @@
+
+pctvar <- function(n, p, c = 100) {
+  n*p/c
+} #n = number (num), p = percentage or ratio (num), c= denominator (num). 
+
+pctvar(25,189)
+pctvar(25,189,1000)
+
+pct <- function(n,p) {
+  n*p/100
+}
+
+
+pctvar(25,189)
+pctvar(25,189,1000)
